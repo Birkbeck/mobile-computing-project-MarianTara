d2ff923a57ec3386cf2d3a5b5dde002c7f4126ff
MarianTara <mtarab01@student.bbk.ac.uk>
2025-05-31 15:47:38 +0100

    fixes after bad commits

--- app/src/main/java/co/uk/bbk/culinarycompanion/MainActivity.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion/MainActivity.kt
@@ -1,7 +1,6 @@
 package co.uk.bbk.culinarycompanion
 
 import android.os.Bundle
-<<<<<<< HEAD
 import android.view.Menu
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,13 +11,12 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Setup ViewBinding and RecyclerView
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Setup RecyclerView (mock only for Coursework 1)
         binding.categoryRecyclerView.layoutManager = LinearLayoutManager(this)
-
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -26,49 +24,3 @@ class MainActivity : ComponentActivity() {
         return true
     }
 }
-=======
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import co.uk.bbk.culinarycompanion.ui.theme.CulinaryCompanionTheme
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CulinaryCompanionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CulinaryCompanionTheme {
-        Greeting("Android")
-    }
-}
->>>>>>> 3a59ac5d1480713034dcf3b67c4e5ef594fedf4e
